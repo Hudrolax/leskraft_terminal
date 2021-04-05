2837acf499f9f24007fd398a233f332b39b341d3
Hudrolax <hudro795@gmail.com>
2021-04-05 13:02:34 +0300

    change logging

--- main.py
+++ main.py
@@ -20,20 +20,24 @@ class Main(LoggerSuper):
 
 if __name__ == '__main__':
     LOG_FORMAT = '%(name)s (%(levelname)s) %(asctime)s: %(message)s'
-    LOG_LEVEL = logging.INFO
+    date_format = '%d.%m.%y %H:%M:%S'
+
+    LOG_LEVEL_WORK = logging.INFO
     if str(LOG_LEVEL).upper() == 'INFO':
-        LOG_LEVEL = logging.INFO
+        LOG_LEVEL_WORK = logging.INFO
     elif str(LOG_LEVEL).upper() == 'DEBUG':
-        LOG_LEVEL = logging.DEBUG
+        LOG_LEVEL_WORK = logging.DEBUG
     elif str(LOG_LEVEL).upper() == 'ERROR':
-        LOG_LEVEL = logging.ERROR
+        LOG_LEVEL_WORK = logging.ERROR
     elif str(LOG_LEVEL).upper() == 'CRITICAL':
-        LOG_LEVEL = logging.CRITICAL
+        LOG_LEVEL_WORK = logging.CRITICAL
     logger = logging.getLogger('main')
 
     if WRITE_LOG_TO_FILE:
-        logging.basicConfig(filename='leskraft_terminal.txt', filemode='w', format=LOG_FORMAT, level=LOG_LEVEL,
-                            datefmt='%d/%m/%y %H:%M:%S')
+        file_log = logging.FileHandler(f'log_{datetime.now().strftime("%m%d%Y%H%M%S")}.txt', mode='a')
+        console_out = logging.StreamHandler()
+        logging.basicConfig(handlers=(file_log, console_out), format=LOG_FORMAT, level=LOG_LEVEL_WORK,
+                            datefmt=date_format)
     else:
         logging.basicConfig(format=LOG_FORMAT, level=LOG_LEVEL, datefmt='%d/%m/%y %H:%M:%S')
 
