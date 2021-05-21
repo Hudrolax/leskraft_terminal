595e376680fc0a304976d0e4fcf311e49d7718ca
Hudrolax <hudro795@gmail.com>
2021-05-21 18:05:10 +0300

    bugfix scan QR

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -45,14 +45,14 @@ class MainController:
         if self.main_window.model.db.get_doc(code) is not None:
             self.open_document_window(code)
         else:
-            self.show_message_with_timer('Документ не найден. Возможно вы осканировали не ту бумагу.')
+            self.show_message_with_timer('Документ не найден. Возможно вы отсканировали не ту бумагу.')
 
     def show_message_with_timer(self, message, color='#960E10', font_size='24'):
         self.main_window._show_error_message(message, color, font_size)
         self._timer = QtCore.QTimer()
         self._timer.setSingleShot(True)
         self._timer.timeout.connect(self.main_window._hide_error_message)
-        self._timer.start(3000)
+        self._timer.start(4000)
 
     def disconnect_scanners_from_main_form(self):
         self.QR_handler.get_QR_code_signal.disconnect(self.get_QR_signal)
